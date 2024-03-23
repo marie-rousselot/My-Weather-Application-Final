f17143558af31c656577943b29c2872480d6c8c9
marie-rousselot <159030042+marie-rousselot@users.noreply.github.com>
2024-03-23 15:23:11 +0800

    injected icon URL

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-
-  console.log(response);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
 
@@ -17,6 +16,10 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  icon.innerHTML = ` <img
+                src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+              />`;
 }
 
 function formatDate(date) {
